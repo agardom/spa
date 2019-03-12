1dd57cfa215e15e852acf01143304382254c2b0c
Alejandro García Domínguez <alejandro@192.168.1.129>
2019-03-12 00:50:38 +0000

    Trabajando en la vista de horarios disponibles y reserva de servicios

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,43 +1,137 @@
-var locale = 'en';
+var gLocale = 'en'
+var gServiceId = -1
+var gServiceName = ''
 
-function drawServices() {
-  var total = services.total;
-  var serviceStr = '';
+function drawServices (pServices) {
+  gServiceId = -1
+  gServiceName = ''
+  var lTotal = pServices.total
+  var lServiceStr = '<div class="row header">' +
+      '<div class="col-xs-2"></div>' +
+      '<div class="col-xs-3" style="font-weight:bold">Servicio</div>' +
+      '<div class="col-xs-6" style="font-weight:bold">Descripcion</div>' +
+    '</div>'
 
-  serviceStr += '<div class="row header">' +
-      '<div class="col-2"></div>' +
-      '<div class="col-3" style="font-weight:bold">Servicio</div>' +
-      '<div class="col-6" style="font-weight:bold">Descripcion</div>'+
-    '</div>';
-
-  var style = "oddRow";
-  for (var i = 0; i < total; i++) {
-    style = i % 2 ? "oddRow" : "evenRow";
-    serviceStr += '<div class="row ' + style + '">' +
-        '<div class="col-1"></div>' +
-        '<div class="col-1"><i class="fa fa-calendar-check campaign-list-info" onclick="selectService(' + services.data[i].id + ', \'' + services.data[i].name +'\');"></i></div>' +
-        '<div class="col-3">' + services.data[i].name + '</div>' +
-        '<div class="col-6"><p>' + services.data[i].description + '</p></div>' +
+  var lStyle = 'oddRow'
+  for (var i = 0; i < lTotal; i++) {
+    lStyle = i % 2 ? 'oddRow' : 'evenRow'
+    lServiceStr += '<div class="row ' + lStyle + '">' +
+        '<div class="col-xs-1"></div>' +
+        '<div class="col-xs-1"><i class="fa fa-arrow-circle-right campaign-list-info" onclick="drawService(' + pServices.data[i].id + ', \'' + pServices.data[i].name + '\');"></i></div>' +
+        '<div class="col-xs-3">' + pServices.data[i].name + '</div>' +
+        '<div class="col-xs-6"><p>' + pServices.data[i].description + '</p></div>' +
       '</div>'
   };
 
-  $('#container').html(serviceStr);
+  $('#container').html(lServiceStr)
 }
 
-function selectService(serviceId, serviceName) {
-  alert(serviceName);
-  $('#serviceSelected').html('Seleccione las fechas entre las que desea buscar disponibilidad para ' + serviceName);
+function drawService (pServiceId, pServiceName) {
+  gServiceId = pServiceId
+  gServiceName = pServiceName
+  var lServiceStr = '<div class="row">' +
+    '<div class="col-xs-1"></div>' +
+    '<div class="col-xs-10 center"><h3>' + gServiceName + '</h3></div>' +
+  '</div>' +
+  '<div class="row">' +
+    '<div class="col-xs-1"></div>' +
+    '<div class="col-xs-10 center"><h5>Seleccione las fechas para buscar disponibilidad</h5></div>' +
+  '</div>' +
+  '<div class="row">' +
+    '<div class="col-xs-2"></div>' +
+    '<div class="col-xs-4 center bold">Fecha inicio</div>' +
+    '<div class="col-xs-4 center bold">Fecha fin</div>' +
+  '</div>' +
+  '<div class="row">' +
+    '<div class="col-xs-2"></div>' +
+    '<div class="col-xs-4 center">' +
+      '<div class="form-group">' +
+        '<div class="input-group date" id="dtpFrom">' +
+          '<input type="text" id="txtDateFrom" class="form-control" />' +
+          '<span class="input-group-addon">' +
+            '<span class="fa fa-calendar"></span>' +
+          '</span>' +
+        '</div>' +
+      '</div>' +
+    '</div>' +
+    '<div class="col-xs-4 center">' +
+      '<div class="form-group">' +
+        '<div class="input-group date" id="dtpTo">' +
+          '<input type="text" id="txtDateTo" class="form-control" />' +
+          '<span class="input-group-addon">' +
+            '<span class="fa fa-calendar"></span>' +
+          '</span>' +
+        '</div>' +
+      '</div>' +
+    '</div>' +
+    '<div class="col-xs-1" id="btnBuscar">' +
+      '<i class="fa fa-search fa-lg campaign-list-info" onclick="loadTimetable(' + gServiceId + ');"></i>' +
+    '</div>' +
+  '</div>'
+
+  $('#container').html(lServiceStr)
+  $('#dtpFrom').datetimepicker({
+    format: 'DD/MM/YYYY',
+    locale: gLocale
+  })
+  $('#dtpTo').datetimepicker({
+    format: 'DD/MM/YYYY',
+    locale: gLocale
+  })
 }
 
-$(function(){
+function drawTimetable (pTimetable) {
+  var lTimetableStr = '<div class="row header">' +
+      '<div class="col-xs-2"></div>' +
+      '<div class="col-xs-3" style="font-weight:bold">Fecha</div>' +
+      '<div class="col-xs-3" style="font-weight:bold">Inicio</div>' +
+      '<div class="col-xs-3" style="font-weight:bold">Fin</div>' +
+    '</div>'
+  var lStyle = ''
+  for (var i in pTimetable) {
+    lStyle = pTimetable[i].booked ? 'booked' : 'available'
+    lTimetableStr += '<div class="row ' + lStyle + '">'
 
-    $(".dropdown-menu a").click(function(){
+    if (pTimetable[i].booked) {
+      lTimetableStr += '<div class="col-xs-2"></div>'
+    }
+    else {
+      lTimetableStr += '<div class="col-xs-1"></div>' +
+        '<div class="col-xs-1"><i class="fa fa-arrow-circle-right campaign-list-info" onclick="alert(\'Reserva de servicio\')"></i></div>'
+    }
 
-      $(".btn.dropdown-toggle:first-child").text($(this).text());
-      $(".btn.dropdown-toggle:first-child").val($(this).text());
+    lTimetableStr += '<div class="col-xs-3">' + pTimetable[i].date + '</div>' +
+        '<div class="col-xs-3">' + pTimetable[i].start + '</div>' +
+        '<div class="col-xs-3">' + pTimetable[i].end + '</div>' +
+      '</div>'
+  }
 
-      locale = $(this).attr("data-locale");
-      getServices(locale);
-   });
+  $('#container').html(lTimetableStr)
+}
 
-});
+function loadTimetable() {
+  var lFrom = $('#txtDateFrom').val().split('/')
+  var lTo = $('#txtDateTo').val().split('/')
+
+  var lDateFrom = new Date(lFrom[2], lFrom[1] - 1, lFrom[0])
+  var lDateTo = new Date(lTo[2], lTo[1] - 1, lTo[0])
+
+  if (!lDateFrom || !lDateTo || lDateFrom > lDateTo) {
+    alert('Las fechas indicadas no son correctas')
+  }
+  else {
+    getAvailableTimetableByHour (gServiceId, lDateFrom, lDateTo)
+  }
+
+  /*var date = new Date();
+  date.yyyymmdd();*/
+}
+
+$(function () {
+  $('.dropdown-menu a').click(function () {
+    $('.btn.dropdown-toggle:first-child').text($(this).text())
+    $('.btn.dropdown-toggle:first-child').val($(this).text())
+    gLocale = $(this).attr('data-locale')
+    getServices(gLocale)
+  })
+})
--- public/js/servicesClient.js
+++ public/js/servicesClient.js
@@ -1,39 +1,80 @@
-function getServices(locale) {
-  alert('Entra en getServices');
-  alert(locale);
-  $.ajax({
-    type: 'GET',
-    dataType: 'json',
-    url: 'http://localhost:8000/api/services',
-    headers: {
-      'Accept':'application/json',
-      'X-localization':locale
-    },
-    success: function(data){
-      drawServices(data);
-    },
-    error: function (request, error) {
-      console.log(arguments);
-      alert('An error has ocurred getting services: ' + error);
-    }
-  });
-};
+var servicesUrl = 'http://localhost:8000/api/'
 
-function getBookings(locale) {
+function getServices (pLocale) {
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    url: 'http://localhost:8000/api/bookings/3/20190210/20180215',
+    url: servicesUrl + 'services',
     headers: {
-      'Accept':'application/json',
-      'X-localization':locale
+      'Accept': 'application/json',
+      'X-localization': pLocale
     },
-    success: function(data){
-      drawBookings(data);
+    success: function (pData) {
+      drawServices(pData)
+    },
+    error: function (pRequest, pError) {
+      console.log(arguments)
+      alert('An error has ocurred getting services: ' + pError)
+    }
+  })
+}
+
+function formatDate (pDate) {
+  var lMonth = '' + (pDate.getMonth() + 1)
+  var lDay = '' + pDate.getDate()
+  var lYear = pDate.getFullYear()
+
+  if (lMonth.length < 2) lMonth = '0' + lMonth
+  if (lDay.length < 2) lDay = '0' + lDay
+
+  return [lYear, lMonth, lDay].join('')
+}
+
+function getAvailableTimetableByHour (pServiceId, pStartDate, pEndDate) {
+  var lStartDate = formatDate(pStartDate)
+  var lEndDate = formatDate(pEndDate)
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: servicesUrl + 'serviceAvailableTimetableByHour/' + pServiceId + '/' + lStartDate + '/' + lEndDate,
+    headers: {
+      'Accept': 'application/json'
+    },
+    success: function (data) {
+      drawTimetable(data)
     },
     error: function (request, error) {
-      console.log(arguments);
-      alert('An error has ocurred getting bookings: ' + error);
+      console.log(arguments)
+      alert('An error has ocurred getting bookings: ' + error)
     }
-  });
+  })
+}
+
+function postBooking (pServiceId, pClient, pComments, pDatetime) {
+
+  var lData = {
+    'clientName': pClient,
+    'comments': pComments,
+    'date': pDatetime,
+    'serviceId': pServiceId,
+    'price': 0
+  }
+
+  $.ajax({
+    type: 'POST',
+    dataType: 'json',
+    data: lData,
+    url: servicesUrl + 'booking',
+    headers: {
+      'Accept': 'application/json'
+    },
+    success: function (pData) {
+      alert('Servicio reservado correctamente')
+    },
+    error: function (pRequest, pError) {
+      console.log(arguments)
+      alert('Ha ocurrido un error al reservar el servicio: ' + pError)
+    }
+  })
+
 }
